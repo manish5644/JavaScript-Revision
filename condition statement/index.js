926834ad7b66cb6790696efe9a381de8e606816e
Manish Dubey <manisdubey.gkp@gmail.com>
2025-07-15 19:00:22 +0530

    condition statement

--- condition statement/index.js
+++ condition statement/index.js
@@ -46,6 +46,18 @@
 // console.log(color);
 
 // 
+// let marks = 75;
+
+// if (marks >= 90) {
+//   console.log("Grade A");
+// } else if (marks >= 75) {
+//   console.log("Grade B");
+// } else if (marks >= 60) {
+//   console.log("Grade C");
+// } else {
+//   console.log("Fail");
+// }
+
 
 
 
