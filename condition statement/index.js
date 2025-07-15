642a61c1453b4240e3770d9c69fac19eb665811c
Manish Dubey <manisdubey.gkp@gmail.com>
2025-07-15 18:54:12 +0530

    condition statement

---
+++ condition statement/index.js
@@ -0,0 +1,51 @@
+// // ************if statement*************
+// let age=18;
+// if(age>=18){
+//     console.log("can vote");
+    
+// }
+
+// let mode="dark";
+// let color;
+// if(mode==="dark"){
+//     color="black";
+//     console.log(color); 
+// }
+
+// // ****************if-else statement********************
+// let mode="light";
+// let color;
+// if(mode==="dark"){
+//     color="black";
+// }
+// else{
+//     color="white";
+// }
+// console.log(color);
+
+// let age=25;
+// if(age<18){
+//     console.log("vote");
+// }
+// else{
+//     console.log("not vote");
+    
+// }
+
+// // *************else-if statement******************
+// let mode="dark";
+// let color;
+// if(mode==="dark"){
+//     color="black";
+// }
+// else if(mode==="blue"){
+//     color="pink";
+// }else{
+//     color="yellow";
+// }
+// console.log(color);
+
+// 
+
+
+
