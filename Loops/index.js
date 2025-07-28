9318c8e14df1bb2aa84ea23b79b4c753f92ae5f5
Manish Dubey <manisdubey.gkp@gmail.com>
2025-07-28 15:23:21 +0530

    loop

---
+++ Loops/index.js
@@ -0,0 +1,44 @@
+// // *************For loop*****************
+
+// let sum =0;
+// for(let i=1; i<=5; i++){
+//     sum=sum+1;
+// }
+// console.log("sum=",sum)
+
+// // *****************While loop*********************
+
+// let i=1;
+// while (i<=5){
+//     console.log("i=",i);
+//     i++;
+// }
+
+// // ****************Do while loop****************
+
+// let i = 1;
+// do{
+//     console.log("i=",i);
+//     i++;
+// }
+// while(i<=5);
+
+// ****************For of loop*****************
+
+// let fruits = ["apple", "mango", "litchi" ,"banana"];
+// for (let fruit of fruits){
+//     console.log("fruit:", fruit);
+// }
+
+// ******************For in loop********************
+
+// let student = {
+//     name:"ravi",
+//     age:23,
+//     city:"gorakhpur",
+//     isPass:"true",
+//     cgpa:9.5
+// };
+// for(let key in student){
+//     console.log(key + ":" , student[key]);
+// }
