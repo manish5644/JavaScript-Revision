866654dcbd9c8664fe96a38a6b519257ab8758e3
Manish Dubey <manisdubey.gkp@gmail.com>
2025-07-28 16:06:46 +0530

    String methods

---
+++ String/index.js
@@ -0,0 +1,43 @@
+// *************** Lenght method*************
+
+// let name ="Manish";
+// console.log(name.length);
+
+// ************** toUppercase() method**********************
+
+// let city ="delhi";
+// console.log(city.toUpperCase());
+
+// *******************toLowercase() method ******************
+
+// let city ="GORAKHPUR";
+// console.log(city.toLowerCase());
+
+// *********************** includes() method**********************
+
+// let sentence ="I love Javascript";
+// console.log(sentence.includes("love"));
+// console.log(sentence.includes("python"));
+
+// ***************** slice(start , end) method ****************************
+
+// let word ="JavaScript";
+// console.log(word.slice(0,4));
+
+// ******************* trim() method *************************
+
+
+// let data = "  hello  ";
+// console.log(data.trim());
+
+// ******************************* CharAt(index) *******************************
+
+// let name = "Manish";
+// console.log(name.charAt(0));
+
+
+// // ****************************** concat()  method *******************************
+
+// let firstName = "Manish";
+// let lastName = "Dubey";
+// console.log(firstName.concat(" ", lastName));
