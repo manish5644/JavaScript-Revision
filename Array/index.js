4bd4b1068a77a98565c2c9fb50e9661eb4de8f78
Manish Dubey <manisdubey.gkp@gmail.com>
2025-07-28 16:49:28 +0530

    array methods

---
+++ Array/index.js
@@ -0,0 +1,73 @@
+// *************************** push() method ****************************
+
+// let name = ["ravi","manish","suraj","nikhil","utkarsh"];
+// name.push("virat");
+// console.log(name);
+
+// *********************** pop() method ***************************
+
+
+// let fruits = ["apple","banana","litchi","strawberry"];
+// fruits.pop();
+// console.log(fruits);
+
+
+// ************************ shift()  method *****************************
+
+
+// let colors = ["red","green","yellow","blue"];
+// colors.shift();
+// console.log(colors);
+
+// **************** unshift()  method *************************************
+
+// let colors = ["red","green","yellow","blue"];
+// colors.unshift("pink");
+// console.log(colors);
+
+
+// // ************************* length method **************************
+
+// let nums = [10,20,30,40];
+// console.log(nums.length);
+
+
+// *********************** indexOf()  method *******************************
+
+// let tools = ["pen","scale","box","eraser"];
+// console.log(tools.indexOf("box"));
+
+
+// *******************  reverse method ******************************
+
+// let nums =  [1,2,3,4,5,67,78,90];
+// nums.reverse();
+// console.log(nums);
+
+
+// ******************** sort()  method****************************
+
+// let nums = [90,80,67,37,2,1,0,];
+// nums.sort();
+// console.log(nums);
+
+
+// **************** concat()  method************************
+
+// let a = [1,2];
+// let b = [3,4];
+// let res = a.concat(b);
+// console.log(res)
+
+
+// // ********************** slice()  method *******************************
+
+// let arr = [10,20,30,40,50];
+// console.log(arr.slice(1,4));
+
+
+// **************** splice()  method **************************
+
+// let arr = [10,20,30,40,50];
+// arr.splice(1,1,78);
+// console.log(arr);
